b0d0d036229b8b7480edbf752cda4b53cea2a9ed
Zuodian Hu <huzuodian@gmail.com>
2019-10-11 17:52:21 -0500

    feat(pwb): add AXIS data strobe

--- pw_bit/hdl/pw_bit.vhd
+++ pw_bit/hdl/pw_bit.vhd
@@ -5,22 +5,21 @@ use ieee.math_real.all;
 
 entity pw_bit_cell is
     generic (
-        COUNTER_WIDTH : integer := 32;
-
-        DATA_AXIS_DATA_WIDTH : integer := 8;
-        CFG_AXIS_DATA_WIDTH  : integer := 32
+        COUNTER_WIDTH   : integer := 32;
+        AXIS_DATA_WIDTH : integer := 8
     );
     port (
         txd : out std_logic;
 
-        data_s_axis_tdata  : in  std_logic_vector(DATA_AXIS_DATA_WIDTH-1 downto 0);
-        data_s_axis_tlast  : in  std_logic;
-        data_s_axis_tvalid : in  std_logic;
-        data_s_axis_tready : out std_logic;
+        s_axis_tdata  : in  std_logic_vector(AXIS_DATA_WIDTH-1 downto 0);
+        s_axis_tstrb  : in  std_logic_vector(AXIS_DATA_WIDTH/8-1 downto 0);
+        s_axis_tlast  : in  std_logic;
+        s_axis_tvalid : in  std_logic;
+        s_axis_tready : out std_logic;
 
-        period  : in std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-        duty_hi : in std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-        duty_lo : in std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
+        period  : in std_logic_vector(COUNTER_WIDTH-1 downto 0);
+        duty_hi : in std_logic_vector(COUNTER_WIDTH-1 downto 0);
+        duty_lo : in std_logic_vector(COUNTER_WIDTH-1 downto 0);
 
         aclk    : in std_logic;
         aresetn : in std_logic
@@ -33,8 +32,8 @@ architecture arch of pw_bit_cell is
     signal tx_state      : state;
     signal tx_state_next : state;
 
-    signal tx_buffer      : std_logic_vector(DATA_AXIS_DATA_WIDTH-1 downto 0);
-    signal tx_buffer_next : std_logic_vector(DATA_AXIS_DATA_WIDTH-1 downto 0);
+    signal tx_buffer      : std_logic_vector(AXIS_DATA_WIDTH-1 downto 0);
+    signal tx_buffer_next : std_logic_vector(AXIS_DATA_WIDTH-1 downto 0);
 
     signal period_reg      : std_logic_vector(COUNTER_WIDTH-1 downto 0);
     signal period_reg_next : std_logic_vector(COUNTER_WIDTH-1 downto 0);
@@ -48,14 +47,47 @@ architecture arch of pw_bit_cell is
     signal bit_time_cnt      : std_logic_vector(COUNTER_WIDTH-1 downto 0);
     signal bit_time_cnt_next : std_logic_vector(COUNTER_WIDTH-1 downto 0);
 
-    constant BIT_CNT_WIDTH : integer := integer(ceil(log2(real(DATA_AXIS_DATA_WIDTH))));
+    constant BIT_CNT_WIDTH : integer := integer(ceil(log2(real(AXIS_DATA_WIDTH))));
+
+    signal bit_cnt_from_tstrb : std_logic_vector(BIT_CNT_WIDTH-1 downto 0);
 
     signal bit_cnt      : std_logic_vector(BIT_CNT_WIDTH-1 downto 0);
     signal bit_cnt_next : std_logic_vector(BIT_CNT_WIDTH-1 downto 0);
 
     signal last      : std_logic;
     signal last_next : std_logic;
+
+    component prio_enc is
+        generic (
+            CODE_WIDTH : integer := 6
+        );
+        port (
+            data : in  std_logic_vector(2**CODE_WIDTH-1 downto 0);
+            code : out std_logic_vector(CODE_WIDTH-1 downto 0)
+        );
+    end component;
+
+    signal bit_cnt_encoder_data : std_logic_vector(2**BIT_CNT_WIDTH-1 downto 0);
+    signal bit_cnt_encoder_code : std_logic_vector(BIT_CNT_WIDTH-1 downto 0);
 begin
+    GEN_ENC_DATA : for i in 0 to AXIS_DATA_WIDTH/8-1 generate
+    begin
+        GEN_ENC_BYTE : for j in 0 to 7 generate
+        begin
+            bit_cnt_encoder_data(8*i+j) <= s_axis_tstrb(i);
+        end generate GEN_ENC_BYTE;
+    end generate GEN_ENC_DATA;
+
+    bit_cnt_encoder : prio_enc
+    generic map (
+        CODE_WIDTH => BIT_CNT_WIDTH
+    ) port map (
+        data => bit_cnt_encoder_data,
+        code => bit_cnt_encoder_code
+    );
+
+    bit_cnt_from_tstrb <= bit_cnt_encoder_code;
+
     process (all) begin
         tx_state_next     <= tx_state    ;
         tx_buffer_next    <= tx_buffer   ;
@@ -68,38 +100,38 @@ begin
 
         case (tx_state) is
             when idle =>
-                txd                       <= '0';
-                data_s_axis_tready        <= '1';
+                txd           <= '0';
+                s_axis_tready <= '1';
 
-                if (data_s_axis_tvalid = '1') then
+                if (s_axis_tvalid = '1') then
                     tx_state_next     <= tx_bit_high;
-                    tx_buffer_next    <= data_s_axis_tdata;
-                    bit_time_cnt_next <= period;
-                    bit_cnt_next      <= (others => '1');
-                    last_next         <= data_s_axis_tlast;
+                    tx_buffer_next    <= s_axis_tdata;
+                    bit_time_cnt_next <= std_logic_vector(unsigned(period)-1);
+                    bit_cnt_next      <= bit_cnt_from_tstrb;
+                    last_next         <= s_axis_tlast;
                 end if;
-                period_reg_next   <= period ;
+                period_reg_next   <= std_logic_vector(unsigned(period)-1) ;
                 high_time_hi_next <= duty_hi;
                 high_time_lo_next <= duty_lo;
 
             when tx_bit_high =>
-                txd                       <= '1';
-                data_s_axis_tready        <= '0';
+                txd           <= '1';
+                s_axis_tready <= '0';
 
                 if (tx_buffer(to_integer(unsigned(bit_cnt))) = '1') then
-                    if (unsigned(bit_time_cnt) < unsigned(period_reg)-unsigned(high_time_hi)) then
+                    if (unsigned(bit_time_cnt) <= unsigned(period_reg)-unsigned(high_time_hi)+1) then
                         tx_state_next <= tx_bit_low;
                     end if;
                 else
-                    if (unsigned(bit_time_cnt) < unsigned(period_reg)-unsigned(high_time_lo)) then
+                    if (unsigned(bit_time_cnt) <= unsigned(period_reg)-unsigned(high_time_lo)+1) then
                         tx_state_next <= tx_bit_low;
                     end if;
                 end if;
                 bit_time_cnt_next <= std_logic_vector(unsigned(bit_time_cnt) - 1);
 
             when tx_bit_low =>
-                txd                       <= '0';
-                data_s_axis_tready        <= '0';
+                txd           <= '0';
+                s_axis_tready <= '0';
 
                 if (unsigned(bit_time_cnt) = 0) then
                     if (unsigned(bit_cnt) = 0) then
@@ -118,8 +150,8 @@ begin
                 end if;
 
             when tx_stop =>
-                txd                       <= '0';
-                data_s_axis_tready        <= '0';
+                txd           <= '0';
+                s_axis_tready <= '0';
 
                 if (unsigned(bit_time_cnt) = 0) then
                     tx_state_next <= idle;
--- pw_bit/sim/pw_bit_tb.sv
+++ pw_bit/sim/pw_bit_tb.sv
@@ -1,53 +1,53 @@
 `timescale 1ns / 1ps
 
 module pw_bit_tb();
-    localparam COUNTER_WIDTH        = 8;
-    localparam DATA_AXIS_DATA_WIDTH = 8;
-    localparam CFG_AXIS_DATA_WIDTH  = 8;
+    localparam COUNTER_WIDTH   = 8;
+    localparam AXIS_DATA_WIDTH = 8;
 
     wire txd; // : out std_logic;
 
-    reg  [DATA_AXIS_DATA_WIDTH-1:0] data_s_axis_tdata ; // : in  std_logic_vector(DATA_AXIS_DATA_WIDTH-1 downto 0);
-    reg                             data_s_axis_tlast ; // : in  std_logic;
-    reg                             data_s_axis_tvalid; // : in  std_logic;
-    wire                            data_s_axis_tready; // : out std_logic;
+    reg  [AXIS_DATA_WIDTH-1:0]   s_axis_tdata ; // : in  std_logic_vector(AXIS_DATA_WIDTH-1 downto 0);
+    reg  [AXIS_DATA_WIDTH/8-1:0] s_axis_tstrb ; // : in  std_logic_vector(AXIS_DATA_WIDTH/8-1 downto 0);
+    reg                          s_axis_tlast ; // : in  std_logic;
+    reg                          s_axis_tvalid; // : in  std_logic;
+    wire                         s_axis_tready; // : out std_logic;
 
-    reg [CFG_AXIS_DATA_WIDTH-1:0] period ; // : in  std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-    reg [CFG_AXIS_DATA_WIDTH-1:0] duty_hi; // : in  std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-    reg [CFG_AXIS_DATA_WIDTH-1:0] duty_lo; // : in  std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
+    reg [COUNTER_WIDTH-1:0] period ; // : in  std_logic_vector(COUNTER_WIDTH-1 downto 0);
+    reg [COUNTER_WIDTH-1:0] duty_hi; // : in  std_logic_vector(COUNTER_WIDTH-1 downto 0);
+    reg [COUNTER_WIDTH-1:0] duty_lo; // : in  std_logic_vector(COUNTER_WIDTH-1 downto 0);
 
     reg aclk   ; // : in std_logic;
     reg aresetn; // : in std_logic
 
     pw_bit_cell #(
-        .COUNTER_WIDTH(COUNTER_WIDTH), // : integer := 32;
-
-        .DATA_AXIS_DATA_WIDTH(DATA_AXIS_DATA_WIDTH), // : integer := 8;
-        .CFG_AXIS_DATA_WIDTH (CFG_AXIS_DATA_WIDTH )  // : integer := COUNTER_WIDTH * 2
+        .COUNTER_WIDTH  (COUNTER_WIDTH)  , // : integer := 32;
+        .AXIS_DATA_WIDTH(AXIS_DATA_WIDTH)  // : integer := 8;
     ) dut (
         .txd(txd), // : out std_logic;
 
-        .data_s_axis_tdata (data_s_axis_tdata ), // : in  std_logic_vector(DATA_AXIS_DATA_WIDTH-1 downto 0);
-        .data_s_axis_tlast (data_s_axis_tlast ), // : in  std_logic;
-        .data_s_axis_tvalid(data_s_axis_tvalid), // : in  std_logic;
-        .data_s_axis_tready(data_s_axis_tready), // : out std_logic;
+        .s_axis_tdata (s_axis_tdata ), // : in  std_logic_vector(AXIS_DATA_WIDTH-1 downto 0);
+        .s_axis_tstrb (s_axis_tstrb ), // : in  std_logic_vector(AXIS_DATA_WIDTH/8-1 downto 0);
+        .s_axis_tlast (s_axis_tlast ), // : in  std_logic;
+        .s_axis_tvalid(s_axis_tvalid), // : in  std_logic;
+        .s_axis_tready(s_axis_tready), // : out std_logic;
 
-        .period (period ), // : in std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-        .duty_hi(duty_hi), // : in std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-        .duty_lo(duty_lo), // : in std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
+        .period (period ), // : in std_logic_vector(COUNTER_WIDTH-1 downto 0);
+        .duty_hi(duty_hi), // : in std_logic_vector(COUNTER_WIDTH-1 downto 0);
+        .duty_lo(duty_lo), // : in std_logic_vector(COUNTER_WIDTH-1 downto 0);
 
         .aclk   (aclk   ), // : in std_logic;
         .aresetn(aresetn)  // : in std_logic
     );
 
     initial begin
-        data_s_axis_tdata  <= 0; // : in  std_logic_vector(DATA_AXIS_DATA_WIDTH-1 downto 0);
-        data_s_axis_tlast  <= 0; // : in  std_logic;
-        data_s_axis_tvalid <= 0; // : in  std_logic;
+        s_axis_tdata  <= 0; // : in  std_logic_vector(AXIS_DATA_WIDTH-1 downto 0);
+        s_axis_tstrb  <= -1; // : in  std_logic_vector(AXIS_DATA_WIDTH/8-1 downto 0);
+        s_axis_tlast  <= 0; // : in  std_logic;
+        s_axis_tvalid <= 0; // : in  std_logic;
 
-        period  <= 0; // : in  std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-        duty_hi <= 0; // : in  std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
-        duty_lo <= 0; // : in  std_logic_vector(CFG_AXIS_DATA_WIDTH-1 downto 0);
+        period  <= 0; // : in  std_logic_vector(COUNTER_WIDTH-1 downto 0);
+        duty_hi <= 0; // : in  std_logic_vector(COUNTER_WIDTH-1 downto 0);
+        duty_lo <= 0; // : in  std_logic_vector(COUNTER_WIDTH-1 downto 0);
 
         aclk    <= 0; // : in std_logic;
         aresetn <= 0; // : in std_logic
@@ -61,16 +61,16 @@ module pw_bit_tb();
 
     always @(posedge(aclk)) begin
         if (aresetn == 1'b0) begin
-            data_s_axis_tdata  <= 8'b11001100;
-            data_s_axis_tlast  <= 1'b1;
-            data_s_axis_tvalid <= 1'b1;
+            s_axis_tdata  <= 8'b11001100;
+            s_axis_tlast  <= 1'b1;
+            s_axis_tvalid <= 1'b1;
     
             period  <= 8'd100;
             duty_hi <= 8'd75;
             duty_lo <= 8'd25;
         end else begin
-            if (data_s_axis_tready == 1'b1) begin
-                data_s_axis_tvalid <= 1'b0;
+            if (s_axis_tready == 1'b1) begin
+                s_axis_tvalid <= 1'b0;
             end
         end
     end
